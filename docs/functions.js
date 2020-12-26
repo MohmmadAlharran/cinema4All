40adbd6f815c1593c9328360b667c03cf88b0379
Elias Kulke <kulkeeli@b-tu.de>
2020-12-26 21:59:59 +0100

    started making home page adaptive

--- docs/functions.js
+++ docs/functions.js
@@ -1,3 +1,16 @@
-function test() {
-	alert("Hi");
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "../resources/movies.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    var names = xmlDoc.getElementsByTagName("title");
+    for (var i = 0; i < names.length; i++) {
+		alert(names[i]);
+	}
 }
