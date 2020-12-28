1896fa48349149e09109f12b5bc43dfd6c31d0ae
Elias Kulke <kulkeeli@b-tu.de>
2020-12-28 13:02:16 +0100

    some new updates

--- docs/functions.js
+++ docs/functions.js
@@ -1,16 +1,30 @@
 var xhttp = new XMLHttpRequest();
 var xmlData;
-var movieMainIndex;
 
 function getMovieListForHome() {
-	if (xmlData == undefined) {		
+	if (xmlData == undefined) {	
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				xmlData = this.responseXML;	
+				movieListHome(0);
+			}	
+		};
+	} else {
+		movieListHome(0);
+	}
+}
+
+function getMovieListForMovies() {
+	if (xmlData == undefined) {	
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				xmlData = this.responseXML;
-				movieListHome(0);
-				
+				alert("hi else");	
+				movieListMovie();
 			}	
 		};
+	} else {
+		movieListHome(0);
 	}
 }
 
@@ -46,9 +60,26 @@ function movieListHome(start) {
 		document.getElementById("movies-list-arrow-left").setAttribute("onclick", "movieListHome(" + (start - 4) + ");");
 		document.getElementById("movies-list-arrow-left").setAttribute("style", "display:unset");
 	}
-	
-	
-	
-	movieMainIndex = 0;
+
 	document.getElementById("movies-main-list").innerHTML = htmlMovieList;
+}
+
+function movieListMovie() {
+	var xmlNames = xmlData.getElementsByTagName("name");
+	var xmlDescriptions = xmlData.getElementsByTagName("description");
+	var xmlVideos = xmlData.getElementsByTagName("video");
+	var xmlRelease = xmlData.getElementsByTagName("release");
+	
+	let htmlMovieList = "";
+	for (var i = 0; i < xmlNames.length; i++) {
+		htmlMovieList += "<li>"
+							+ "<a href='##>"
+								+ "<h2>" + xmlNames[i].childNodes[0].nodeValue + "(" + xmlRelease[i].childNodes[0].nodeValue +  "</h2>"
+								+ "<iframe width='560' height='315' src='" + xmlVideos[i].childNodes[0].nodeValue + "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+								+ "<div>" + xmlDescriptions[i].childNodes[0].nodeValue + "</div>"
+							 + "</a>"
+						+ "</li>";
+	}
+	
+	document.getElementById("movies-movie-list").innerHTML = htmlMovieList;
 }
